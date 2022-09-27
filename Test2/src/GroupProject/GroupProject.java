ef529df11272d89166f53658d114344fcb5df54c
ericowens <ericowens@10.250.10.87>
2022-09-27 16:36:03 -0400

    hi

--- Test2/src/GroupProject/GroupProject.java
+++ Test2/src/GroupProject/GroupProject.java
@@ -1,15 +1,24 @@
 package GroupProject;
 
 public class GroupProject {
-
+    
+    	
+   
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+int i = 2;
 
-	
-	int i = 0;
-	int y = 9;
-    System.out.println(i + y);
-	
+switch(i) {
+case 0:
+	System.out.println("s");
+	break;
+case 1:
+System.out.println("e");
+break;
+case 2:
+	System.out.println("e");
+	break;
+}
 	
 	
 	
